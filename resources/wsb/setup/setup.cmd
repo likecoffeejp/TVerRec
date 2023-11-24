7860758ad173f7330f629094ee5498e45a3c4511
likecoffee <likecoffeejp@gmail>
2023-11-25 06:11:00 +0900

    2.9.9 反映

--- resources/wsb/setup/setup.cmd
+++ resources/wsb/setup/setup.cmd
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 powershell Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 
 rem Download and Install WinGet & some more packages
 powershell "C:\users\WDAGUtilityAccount\setup\setup.ps1"
 
-=======
-powershell Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
-
-rem Download and Install WinGet & some more packages
-powershell "C:\users\WDAGUtilityAccount\setup\setup.ps1"
-
->>>>>>> 7d8dd8d50f67eab0736d51f4b046297c1e0f86b8
--- resources/wsb/setup/tverrec.cmd
+++ resources/wsb/setup/tverrec.cmd
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 rem Clone TVerRec
 cd C:\Users\WDAGUtilityAccount\Desktop
 "C:\Program Files\Git\cmd\git.exe" clone https://github.com/dongaba/TVerRec
 
 explorer.exe C:\Users\WDAGUtilityAccount\Desktop\TVerRec\win
 
-=======
-rem Clone TVerRec
-cd C:\Users\WDAGUtilityAccount\Desktop
-"C:\Program Files\Git\cmd\git.exe" clone https://github.com/dongaba/TVerRec
-
-explorer.exe C:\Users\WDAGUtilityAccount\Desktop\TVerRec\win
-
->>>>>>> 7d8dd8d50f67eab0736d51f4b046297c1e0f86b8
